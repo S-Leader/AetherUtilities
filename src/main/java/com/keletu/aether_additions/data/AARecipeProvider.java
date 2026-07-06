08ea9536dbc5a23701ec20d4636b23c020e58611
a <EYCHFSJD@gmail.com>
2026-07-06 13:40:51 +0800

    small fix.

--- src/main/java/com/keletu/aether_additions/data/AARecipeProvider.java
+++ src/main/java/com/keletu/aether_additions/data/AARecipeProvider.java
@@ -1,6 +1,7 @@
 package com.keletu.aether_additions.data;
 
 import com.aetherteam.aether.block.AetherBlocks;
+import com.aetherteam.aether.data.providers.AetherRecipeProvider;
 import com.aetherteam.aether.item.AetherItems;
 import com.keletu.aether_additions.AetherAdditions;
 import com.keletu.aether_additions.item.AetherCrossbowItems;
@@ -8,7 +9,6 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
@@ -17,9 +17,10 @@ import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
 
-public class AARecipeProvider extends RecipeProvider {
-    public AARecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
-        super(output, registries);
+public class AARecipeProvider extends AetherRecipeProvider {
+
+    public AARecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(output, lookupProvider, AetherAdditions.MODID);
     }
 
     @Override
@@ -28,17 +29,15 @@ public class AARecipeProvider extends RecipeProvider {
         crossbow(output, AetherCrossbowItems.HOLYSTONE_CROSSBOW.get(), AetherBlocks.HOLYSTONE.get(), "holystone_crossbow");
         crossbow(output, AetherCrossbowItems.ZANITE_CROSSBOW.get(), AetherItems.ZANITE_GEMSTONE.get(), "zanite_crossbow");
         crossbow(output, AetherCrossbowItems.GRAVITITE_CROSSBOW.get(), AetherBlocks.ENCHANTED_GRAVITITE.get(), "gravitite_crossbow");
+
+        this.repairingRecipe(RecipeCategory.COMBAT, AetherCrossbowItems.SKYROOT_CROSSBOW.get(), 250).group("altar_corssbow_repair").save(output, this.name("skyroot_corssbow_repairing"));
+        this.repairingRecipe(RecipeCategory.COMBAT, AetherCrossbowItems.HOLYSTONE_CROSSBOW.get(), 500).group("altar_corssbow_repair").save(output, this.name("holystone_corssbow_repairing"));
+        this.repairingRecipe(RecipeCategory.COMBAT, AetherCrossbowItems.ZANITE_CROSSBOW.get(), 750).group("altar_corssbow_repair").save(output, this.name("zanite_corssbow_repairing"));
+        this.repairingRecipe(RecipeCategory.COMBAT, AetherCrossbowItems.GRAVITITE_CROSSBOW.get(), 1500).group("altar_corssbow_repair").save(output, this.name("gravitite_corssbow_repairing"));
+
     }
 
     private static void crossbow(RecipeOutput output, ItemLike result, ItemLike material, String id) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, result)
-                .pattern("SMS")
-                .pattern("PTP")
-                .pattern(" S ")
-                .define('S', AetherItems.SKYROOT_STICK.get())
-                .define('M', material).define('P', Tags.Items.STRINGS)
-                .define('T', Items.TRIPWIRE_HOOK).group("aether_crossbows")
-                .unlockedBy("has_" + getItemName(material), has(material)).unlockedBy("has_tripwire_hook", has(Items.TRIPWIRE_HOOK))
-                .save(output, ResourceLocation.fromNamespaceAndPath(AetherAdditions.MODID, id));
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, result).pattern("SMS").pattern("PTP").pattern(" S ").define('S', AetherItems.SKYROOT_STICK.get()).define('M', material).define('P', Tags.Items.STRINGS).define('T', Items.TRIPWIRE_HOOK).group("aether_crossbows").unlockedBy("has_" + getItemName(material), has(material)).unlockedBy("has_tripwire_hook", has(Items.TRIPWIRE_HOOK)).save(output, ResourceLocation.fromNamespaceAndPath(AetherAdditions.MODID, id));
     }
 }
